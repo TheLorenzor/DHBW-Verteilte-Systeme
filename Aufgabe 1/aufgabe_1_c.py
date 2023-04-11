2473ea217bc05a6f376c42ccebdc6be1955a1675
the_loraxxz <the_loraxxz@web.de>
2023-04-11 11:10:32 +0200

    Finished mqtt reading

--- Aufgabe 1/aufgabe_1_c.py
+++ Aufgabe 1/aufgabe_1_c.py
@@ -1,8 +1,22 @@
+import json
+
 import paho.mqtt.client as mqtt
 
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("/weather/#")
 
-def mqtt_reading() -> dict:
+def mqtt_reading(client, userdata, msg):
+    weather_obj = json.loads(msg.payload)
+    mqtt_output(msg.topic,weather_obj)
+
+def mqtt_output(topc:str,dictionary:dict):
     pass
 
-def mqtt_output():
-    pass
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = mqtt_reading
+
+client.connect("10.50.12.150", 1883, 60)
+
+client.loop_forever()
