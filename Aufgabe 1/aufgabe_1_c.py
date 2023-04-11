6dd24f81fe03ffea82eeeb2e6a8bd7e50cc716ac
Faistos <faistos01@gmail.com>
2023-04-11 11:15:43 +0200

    mqtt_output

--- Aufgabe 1/aufgabe_1_c.py
+++ Aufgabe 1/aufgabe_1_c.py
@@ -10,7 +10,20 @@ def mqtt_reading(client, userdata, msg):
     mqtt_output(msg.topic,weather_obj)
 
 def mqtt_output(topc:str,dictionary:dict):
-    pass
+    if topc.startswith("/weather"):
+        city = dictionary.get("city")
+        temp_current = dictionary.get("tempCurrent")
+        temp_max = dictionary.get("tempMax")
+        temp_min = dictionary.get("tempMin")
+        comment = dictionary.get("comment")
+        timestamp = dictionary.get("timeStamp")
+
+        print(f"Weather data for {city} at {timestamp}:")
+        print(f"Current temperature: {temp_current}")
+        print(f"Max temperature: {temp_max}")
+        print(f"Min temperature: {temp_min}")
+        print(f"Comment: {comment}")
+        print("-----------")
 
 
 client = mqtt.Client()
