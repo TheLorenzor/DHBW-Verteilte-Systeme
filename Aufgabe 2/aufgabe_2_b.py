ed60f5c223f546e11ca39dfc626ad87251231fff
The Loraxxz <tilmann.lorenz@mail.com>
2023-04-28 09:53:39 +0200

    finished 2b

--- Aufgabe 2/aufgabe_2_b.py
+++ Aufgabe 2/aufgabe_2_b.py
@@ -1,1 +1,22 @@
+from confluent_kafka import Consumer
 
+config = {
+    'bootstrap.servers': '10.50.15.52:9092',
+    'auto.offset.reset': 'latest',
+    'group.id': 'the mighty avengers',
+
+}
+
+cons = Consumer(config)
+
+cons.subscribe(['weather'])
+
+while True:
+    msg = cons.poll(1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        break
+    print(msg.value().decode('utf-8'))
+
+cons.close()
