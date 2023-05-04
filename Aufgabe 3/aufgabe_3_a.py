62ef25b5f355b61b167ce37af61dd4b942362733
Faistos <faistos01@gmail.com>
2023-05-04 14:34:17 +0200

    Changes by timSc

--- Aufgabe 3/aufgabe_3_a.py
+++ Aufgabe 3/aufgabe_3_a.py
@@ -1,27 +1,32 @@
+from confluent_kafka import Consumer, KafkaError, TopicPartition
 
+# Set up consumer properties
+bootstrap_servers = '10.50.15.52:9092'
+group_id = 'the_mighty_avengers'
+topic_name = 'tankerkoenig'
 
-from confluent_kafka import Consumer
-config = {
-    'bootstrap.servers': '10.50.15.52:9092',
-    'auto.offset.reset': 'latest',
-    'group.id': 'the_mighty_avengers',
+# Create a KafkaConsumer object
+consumer = Consumer({
+    'bootstrap.servers': bootstrap_servers,
+    'group.id': group_id,
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': False
+})
 
-}
+# Manually assign partitions to the consumer
+partition0 = TopicPartition(topic_name, 0)
+partition1 = TopicPartition(topic_name, 1)
+consumer.assign([partition0, partition1])
 
-# instantiate the consumer with config and subscribe to the weather channel
-# establish the connection to graphite
-cons = Consumer(config)
-cons.subscribe(['tankerkoenig'])
-
-
+# Start consuming data
 while True:
-    msg = cons.poll(1.0)
-    print("abc")
+
+    msg = consumer.poll(1.0)
     # ignore msgs that are empty
     if msg is None or msg.error():
         continue
     # listen for data
     print(msg.value().decode('utf-8'))
-
-# close connection
-cons.close()
+    # Commit the offsets of the processed messages
+    consumer.commit()
+consumer.close()
